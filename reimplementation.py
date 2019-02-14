cb19173d14f86a43b461326749c7867c54e080fd
Naomi <naomi.t.voerman@gmail.com>
2019-02-14 11:47:49 +0100

    Can manually implement the replicate numbers that are not clear from the data. Takes this list and adds it to both sheets of the original dataset (including the meta data).

--- reimplementation.py
+++ reimplementation.py
@@ -1,5 +1,6 @@
 import math
 import pickle
+import csv
 from collections import Counter, defaultdict, OrderedDict
 
 import matplotlib.pyplot as plt
@@ -555,6 +556,7 @@ if __name__ == '__main__':
     model_file_name = 'mlpmodel'
     if from_penile:
         model_file_name+='_penile'
+    unknown_replicatenumbers = False
 
     # which classes should we compute marginals for? all single cell types and a 'contains vaginal' class?
     # '-1' to avoid the penile skin
@@ -662,34 +664,51 @@ if __name__ == '__main__':
     ### Naomi's part ###
     # Assign the correct replicates to the same sample for the single body fluids.
     sheet_to_df_map = read_original_data('Datasets/Dataset_NFI_adj.xlsx')
-    relevant_column = list(zip(*list(sheet_to_df_map['Samples + details'].index)))[3]
-    shortened_names = [relevant_column[i][-3:] for i in range(len(relevant_column))]
+    if unknown_replicatenumbers:
+        relevant_column = list(zip(*list(sheet_to_df_map['Samples + details'].index)))[3]
+        shortened_names = [relevant_column[i][-3:] for i in range(len(relevant_column))]
 
-    replicate_values = OrderedDict()
-    indexes_to_be_checked = []
-    for i in range(len(shortened_names)):
-        try:
-            replicate_values[i] = int(shortened_names[i][-1])
-        except ValueError:
-            indexes_to_be_checked.append(i)
-            replicate_values[i] = shortened_names[i]
-        if shortened_names[i] in ['0.3', '.75', '0.5', 'a_1', 'n_1', 'n_4']:
-            indexes_to_be_checked.append(i)
-            replicate_values[i] = shortened_names[i]
-        if shortened_names[i][-1] in ['0', '6']:
-            indexes_to_be_checked.append(i)
-            replicate_values[i] = shortened_names[i]
+        replicate_values = OrderedDict([])
+        indexes_to_be_checked = []
+        for i in range(len(shortened_names)):
+            try:
+                replicate_values[i] = int(shortened_names[i][-1])
+            except ValueError:
+                indexes_to_be_checked.append(i)
+                replicate_values[i] = shortened_names[i]
+            if shortened_names[i] in ['0.3', '.75', '0.5', 'a_1', 'n_1', 'n_4']:
+                indexes_to_be_checked.append(i)
+                replicate_values[i] = shortened_names[i]
+            if shortened_names[i][-1] in ['0', '6']:
+                indexes_to_be_checked.append(i)
+                replicate_values[i] = shortened_names[i]
 
-    # TODO: adjust the shortened_names
-    # TODO: iterate over the index and manually adjust
-    # if str --> 1
-    # if ends with either 0.3, 0.75, 0.5 --> check
-    # if ends with a 6 --> check
-    # if ends with a 0 --> 1
-    # if ends with _1 --> check
+        # Iterate over the index and manually adjust
+        replace_replicate = []
+        for i in range(len(indexes_to_be_checked)):
+            print("The middle rowname of the following rownames should be adjusted:",
+                  relevant_column[indexes_to_be_checked[i]-1:indexes_to_be_checked[i]+2])
+            replace_replicate.append(getNumeric("Give the replicate number"))
 
-    k = getNumeric("Give the number of buckets:")
+        for i in range(len(indexes_to_be_checked)):
+            replicate_values[indexes_to_be_checked[i]] = replace_replicate[i]
 
-    for i in indexes_to_be_checked:
-        print("The rowname in the excel file is:", relevant_column[i])
+        replicates = list(replicate_values.values())
+        replicates.insert(0, "replicate value")
+        csvfile = "Datasets/replicate_numbers_single.csv"
 
+        # Assuming res is a flat list
+        with open(csvfile, "w") as output:
+            writer = csv.writer(output, lineterminator='\n')
+            for val in replicates:
+                writer.writerow([val])
+
+    sheet_to_df_map['Samples + details'].to_csv('Datasets/Dataset_NFI_meta.csv', encoding='utf-8')
+    sheet_to_df_map['Data uitgekleed'].to_csv('Datasets/Dataset_NFI_adj.csv', encoding='utf-8')
+    a = pd.read_csv("Datasets/Dataset_NFI_meta.csv", delimiter=',')
+    b = pd.read_csv("Datasets/Dataset_NFI_adj.csv", delimiter=',')
+    c = pd.read_csv("Datasets/replicate_numbers_single.csv")
+    mergedac = pd.concat([a, c], axis=1)
+    mergedbc = pd.concat([b, c], axis=1)
+    mergedac.to_csv("Datasets/Dataset_NFI_meta.csv", sep=';', index=False, float_format='%.f')
+    mergedbc.to_csv("Datasets/Dataset_NFI_adj.csv", sep=';', index=False, float_format='%.f')
