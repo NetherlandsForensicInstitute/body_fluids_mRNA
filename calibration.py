9342a3632a40ce98e59eae979ba01803a7cee2b6
Naomi <naomi.t.voerman@gmail.com>
2019-02-26 19:17:45 +0100

    Plots the true loglrs against the expected lrs

--- calibration.py
+++ calibration.py
@@ -13,7 +13,7 @@ import numpy as np
 from operator import itemgetter
 
 from reimplementation import get_data_per_cell_type, read_mixture_data, \
-    calculate_lrs, combine_samples
+    calculate_lrs
 
 from lir.calibration import KDECalibrator
 
@@ -34,11 +34,7 @@ if __name__ == '__main__':
     lrs_per_class = calculate_lrs(
         X_mixtures, model, mixture_classes_in_classes_to_evaluate, n_features, MAX_LR, log=False)
 
-    # TODO: split the data in two parts --> train and calibration set
-    # TODO: augment both sets
-    # TODO: train the model
-    # TODO: calculate the scores of the trained model
-    # TODO: plot the log_lrs_per_class per sample: histograms
+    # Plot the log_lrs_per_class per sample: histograms
     n_per_mixture_class = collections.Counter(y_mixtures)
     unique_groups_mixt, indices = np.unique(y_mixtures, return_index=True)
     sorted_test_map = collections.OrderedDict(sorted(n_per_mixture_class.items()))
@@ -47,13 +43,8 @@ if __name__ == '__main__':
     sorted_classes_map = collections.OrderedDict(sorted(classes_map.items(), key=itemgetter(1)))
     names_single = np.array([list(sorted_classes_map.keys())])
 
-    sorted_test_map = collections.OrderedDict(sorted(inv_test_map.items()))
-    names_mixt = np.array([list(sorted_test_map.values())]).flatten()[np.argsort(indices)]
-    names_mixt = np.reshape(names_mixt, (7, 1))
-
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'orange']
     for j, j_class in enumerate(set(y_mixtures)):
-        print(j_class)
         indices_experiments = [k for k in range(len(y_mixtures)) if y_mixtures[k] == j_class]
         plt.subplots(2, 5, figsize=(18, 9))
         plt.suptitle(sorted_test_map[j_class], y=1.0, fontsize=14)
@@ -65,6 +56,28 @@ if __name__ == '__main__':
             plt.title(names_single[0][i])
         plt.tight_layout()
         plt.show()
+
+    # TODO: get the correct h1 and h2: put in function
+    # h1 are all the LRs from a mixt. cell types in which a specific cell type exists
+    # h2 are all the LRs from a mixt. cell types in which the specific cell type is not
+
+    # TODO: check whether the plotted values are correct --> sorted(log_lrs)
+    plt.subplots(2, 5, figsize=(18, 9))
+    length_lrs = log_lrs_per_class.shape[0]
+    for i in range(log_lrs_per_class.shape[1]):
+        plt.subplot(2, 5, i + 1)
+        plt.plot(np.linspace(-MAX_LR - .5, MAX_LR + .5, length_lrs),
+                 np.linspace(-MAX_LR - .5, MAX_LR + .5, length_lrs),
+                 color='k')
+        plt.plot(sorted(log_lrs_per_class[:, i]),
+                 np.linspace(-MAX_LR - .5, MAX_LR + .5, length_lrs))
+        plt.title(names_single[0][i])
+    #plt.show()
+    plt.savefig('reliability_plots.png')
+
+
+
+
 
 
 
