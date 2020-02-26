remove DL, fix coef of log regr (calibration/softmax)

rna/analytics.py
@@ -5,7 +5,7 @@ Performs project specific.
 import os
 from collections import OrderedDict
 
-import keras
+# import keras
 import numpy as np
 from lir.lr import calculate_cllr
 from sklearn.metrics import accuracy_score
@@ -44,10 +44,13 @@ def generate_lrs(X_train, y_train, X_calib, y_calib, X_test, X_test_as_mixtures,
             # y_test = mle.nhot_to_labels(y_test)
         except:  # already are labels
             pass
-        # for DL model y_train must always be nhot encoded
-        # TODO: Find better solution
-        if isinstance(model._classifier, keras.engine.training.Model):
-            y_train = np.eye(2 ** 8)[y_train]
+        try:
+            # for DL model y_train must always be nhot encoded
+            # TODO: Find better solution
+            if isinstance(model._classifier, keras.engine.training.Model):
+                y_train = np.eye(2 ** 8)[y_train]
+        except:
+            pass
     else:  # y_train must be nhot encoded labels
         try:
             y_train = mle.labels_to_nhot(y_train)
@@ -95,7 +98,7 @@ def generate_lrs(X_train, y_train, X_calib, y_calib, X_test, X_test_as_mixtures,
            lrs_before_calib_mixt, lrs_after_calib_mixt
 
 
-def clf_with_correct_settings(clf_no_settings, softmax: bool, n_classes):
+def clf_with_correct_settings(clf_no_settings, softmax: bool, n_classes: int, with_calibration: bool):
     """
     Ensures that the correct classifier with correct settings is used in the analysis. This is based on a string
     'model_no_settings' and a boolean deciding how the probabilties are calculated 'softmax': either with the softmax
@@ -163,7 +166,10 @@ def perform_analysis(X_train_augmented, y_train_nhot_augmented, X_calib_augmente
     classifier = models[0]
     with_calibration = models[1]
 
-    model = clf_with_correct_settings(classifier, softmax, n_classes=target_classes.shape[0])
+    model = clf_with_correct_settings(classifier, softmax, n_classes=target_classes.shape[0], with_calibration=with_calibration)
+    if not with_calibration:
+        # hacky, nicer to do at instantiation
+        model._calibrator=None
 
     if with_calibration:  # with calibration
         model, lrs_before_calib, lrs_after_calib, lrs_before_calib_test_as_mixtures, lrs_after_calib_test_as_mixtures, \
@@ -322,16 +328,19 @@ def calculate_accuracy_all_target_classes(X, y_true, target_classes, model, mle)
     """
 
     y_pred = model._classifier.predict(X)
-    if isinstance(model._classifier, keras.engine.training.Model):
-        # when the model predicts probabilities rather than the binary classes
-        # this is only the case for the DL model
-        if y_pred.shape[1] == 2 ** 8:
-            unique_vectors = np.flip(np.unique(nhot_matrix_all_combinations, axis=0), axis=1)
-            y_pred = np.array([np.sum(y_pred[:, np.argwhere(unique_vectors[:, i] == 1).flatten()], axis=1) for i in
-                               range(unique_vectors.shape[1])]).T
-            y_pred = np.where(y_pred > 0.5, 1, 0)
-        else:
-            y_pred = np.where(y_pred > 0.5, 1, 0)
+    try:
+        if isinstance(model._classifier, keras.engine.training.Model):
+            # when the model predicts probabilities rather than the binary classes
+            # this is only the case for the DL model
+            if y_pred.shape[1] == 2 ** 8:
+                unique_vectors = np.flip(np.unique(nhot_matrix_all_combinations, axis=0), axis=1)
+                y_pred = np.array([np.sum(y_pred[:, np.argwhere(unique_vectors[:, i] == 1).flatten()], axis=1) for i in
+                                   range(unique_vectors.shape[1])]).T
+                y_pred = np.where(y_pred > 0.5, 1, 0)
+            else:
+                y_pred = np.where(y_pred > 0.5, 1, 0)
+    except:
+        pass
 
     try:
         y_true = mle.labels_to_nhot(y_true)

rna/plotting.py
@@ -460,6 +460,7 @@ def plot_boxplot_of_metric(binarize, softmax, models, priors, n_metric, label_en
     ax.set_yticks(range(total_boxplots))
     ax.set_yticklabels(names)
     ax.set_ylim(-0.5, total_boxplots-0.5)
+    ax.set_xlim(0, 1)
     ax.set_xlabel(name_metric)
 
     if savefig is not None:
@@ -926,15 +927,26 @@ def plot_coefficient_importances(model, target_classes, present_markers, label_e
         target_class_str = vec2string(target_class, label_encoder)
         celltype = target_class_str.split(' and/or ')
         # TODO: Is this correct for MLP with softmax?
+        # NO
         if len(model._classifier.coef_) == 2 ** 8:
+            # the marginal takes the sum over many probabilities. taking the log does not yield anything nice it seems
+            # (although the mean will probably correlate)
+            return
             indices_target_class = get_mixture_columns_for_class(target_class, None)
             intercept = np.mean(model._classifier.intercept_[indices_target_class])
             coefficients = np.mean(model._classifier.coef_[indices_target_class, :], axis=0)
         else:
-            intercept = model._classifier.intercept_[t, :]
-            coefficients = model._classifier.coef_[t, :]
+            intercept = model._classifier.intercept_[t, :].squeeze()/np.log(10)
+            coefficients = model._classifier.coef_[t, :].squeeze()/np.log(10)
 
-        plot_coefficient_importance(intercept, coefficients, present_markers, celltype)
+
+        if model._calibrator:
+            beta1=model._calibrators_per_target_class[str(target_class)]._logit.coef_[0][0]/np.log(10)
+            beta0=model._calibrators_per_target_class[str(target_class)]._logit.intercept_[0]/np.log(10)
+            plot_coefficient_importance(intercept*beta1+beta0, coefficients*beta1, present_markers, celltype)
+        else:
+            plot_coefficient_importance(intercept, coefficients, present_markers, celltype)
+
 
         target_class_save = target_class_str.replace(" ", "_")
         target_class_save = target_class_save.replace(".", "_")
@@ -954,29 +966,18 @@ def plot_coefficient_importance(intercept, coefficients, present_markers, cellty
     """
 
     :param intercept:
-    :param coefficients:
+    :param coefficients: these are assumed to already be transformed to base 10
     :param present_markers:
     :param celltypes: list of strings: list of celltypes
     :return:
     """
 
-    def calculate_maximum_lr(intercept, coefficients):
-        positive_coefficients = coefficients[np.argwhere(coefficients > 0).ravel()]
-        all_coefficients = np.append(intercept, positive_coefficients)
-        max_probability = 1 / (1 + np.exp(-(np.sum(all_coefficients))))
-        max_lr = max_probability / (1 - max_probability)
-        if max_lr > 10 ** 10:
-            return np.log10(10 ** 10)
-        else:
-            return np.log10(max_lr)
+    def calculate_max_base_log_lr(intercept, coefficients):
+        return np.sum([coef for coef in coefficients.squeeze() if coef > 0] + [intercept])
 
     coefficients = np.reshape(coefficients, -1)
-    max_lr = calculate_maximum_lr(intercept, coefficients)
 
-    # change values of coefficients into interpretable scale --> log10
-    coefficients = [1/(1+np.exp(-(coefficient))) for coefficient in coefficients]
-    coefficients = [coefficient / (1 - coefficient) for coefficient in coefficients]
-    coefficients = np.log10(coefficients)
+    max_base = calculate_max_base_log_lr(intercept, coefficients)
 
     # sort
     sorted_indices = np.argsort(coefficients)
@@ -1003,7 +1004,7 @@ def plot_coefficient_importance(intercept, coefficients, present_markers, cellty
         pass
     plt.yticks(x, present_markers)
 
-    plt.title('Max 10log LR = {}'.format(math.ceil(max_lr)))
+    plt.title('Max, base 10log LR = {}, {}'.format(max_base, intercept))
     plt.xlabel('10log Coefficient value')
     plt.ylabel('Marker names')
 